3c8755dbd8cf5764dbd538c8126941c6624933ce
Guilherme Bodart <guilhermebodart73@gmail.com>
2023-12-09 23:36:24 -0300

    trocando para produção

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const uri_test = "mongodb+srv://outrigger:Dwu5cRsfqmF60oup@cluster0.rda29.gcp.mongodb.net/testCurriculum?retryWrites=true&w=majority";
 const uri_production = "mongodb+srv://outrigger:Dwu5cRsfqmF60oup@cluster0.rda29.gcp.mongodb.net/productionCurriculum?retryWrites=true&w=majority";
 
-mongoose.connect(uri_test, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri_production, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
