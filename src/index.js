e88507d8e4e4eae232db6e09357489bb7e2b5312
Guilherme Bodart <guilhermebodart73@gmail.com>
2023-12-10 00:07:40 -0300

    vercel json

--- src/index.js
+++ src/index.js
@@ -1,35 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// const passport = require("passport");
-const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
 
 const app = express();
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.serializeUser(function (user, cb) {
-//   cb(null, user);
-// });
-
-// passport.deserializeUser(function (obj, cb) {
-//   cb(null, obj);
-// });
-
-// passport.use(
-//   new LinkedInStrategy(
-//     {
-//       clientID: config.linkedinAuth.clientID,
-//       clientSecret: config.linkedinAuth.clientSecret,
-//       callbackURL: config.linkedinAuth.callbackURL,
-//       scope: ["r_emailaddress", "r_liteprofile"],
-//     },
-//     function (token, tokenSecret, profile, done) {
-//       return done(null, profile);
-//     }
-//   )
-// );
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
